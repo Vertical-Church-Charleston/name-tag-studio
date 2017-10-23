e7063cd5a4a85ab3d6c40b7e045abc97f97fecca
Jordan Riser <riser.jordan@gmail.com>
2017-10-23 15:20:37 -0400

    test for name ti be rendered

--- tests/integration/components/tag-component-test.js
+++ tests/integration/components/tag-component-test.js
@@ -5,20 +5,23 @@ moduleForComponent('tag-component', 'Integration | Component | tag component', {
   integration: true
 });
 
-test('it renders', function(assert) {
+test('it renders first and last name', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('data',{firstName:"Jordan",lastName:"Riser"});
 
-  this.render(hbs`{{tag-component}}`);
+  this.render(hbs`{{tag-component data=data}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$().find('.first-name').text(), 'Jordan');
+  assert.equal(this.$().find('.last-name').text(), 'Riser');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#tag-component}}
-      template block text
-    {{/tag-component}}
-  `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  // // Template block usage:
+  // this.render(hbs`
+  //   {{#tag-component}}
+  //     template block text
+  //   {{/tag-component}}
+  // `);
+
+  // assert.equal(this.$().text().trim(), 'template block text');
 });
