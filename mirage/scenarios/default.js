fa65b069556406ea70f14edae0fd7d3a4e039441
Jordan Riser <riser.jordan@gmail.com>
2017-10-23 19:51:18 -0400

    tweak layout and spacing styles for tags

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -5,5 +5,5 @@ export default function(server) {
     This data will not be loaded in your tests.
   */
 
-  server.createList('tag', 2,{template:1});
+  server.createList('tag', 12,{template:1});
 }
