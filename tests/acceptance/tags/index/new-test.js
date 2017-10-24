f1ddd2445f9e6d66371a508b89c1ee7c3bb427f3
Jordan Riser <riser.jordan@gmail.com>
2017-10-24 14:42:25 -0400

    adding base content for new tag page

--- tests/acceptance/tags/index/new-test.js
+++ tests/acceptance/tags/index/new-test.js
@@ -3,10 +3,11 @@ import moduleForAcceptance from 'name-tags/tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | tags/index/new');
 
-test('visiting /tags/index/new', function(assert) {
+test('should render a form section and display section', function(assert) {
   visit('/tags/new');
 
   andThen(function() {
-    assert.equal(currentURL(), '/tags/new');
+    assert.equal( find('.input-section').length, 1 );
+    assert.equal( find('.display-section').length, 1 );
   });
 });
