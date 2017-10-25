dcdc4ee4c983e923176a1541a4c29ae798291323
Jordan Riser <riser.jordan@gmail.com>
2017-10-25 12:26:22 -0400

    add notify service

--- app/routes/tags/list/edit.js
+++ app/routes/tags/list/edit.js
@@ -1,6 +1,8 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  notify: service(),
   model(params){
     return this.store.find('tag',params.id);
   },
@@ -9,6 +11,9 @@ export default Route.extend({
       if (reason.status == 404)
         return true
       else
+        this.get('notify').error('This tag does not exist',{
+          closeAfter: 10000
+        });
         this.transitionTo('tags.list');
     },
   },
