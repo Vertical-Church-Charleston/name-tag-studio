13a1cfd91611c8cb49a4e01ca02d6679629301ac
Jordan Riser <riser.jordan@gmail.com>
2017-10-25 14:02:49 -0400

    remove commented asserts

--- tests/acceptance/tags/list/new-test.js
+++ tests/acceptance/tags/list/new-test.js
@@ -40,7 +40,5 @@ test('should push record into print list on save',function(assert){
     assert.equal(controller.get('printList.length'),1,'pushes the tag into the array');
     assert.equal(find('.display-section .print-list .count').text().trim(),'1','displays the count of the print list');
     assert.equal(find('input').val(),'','clears inputs for next input');
-    // assert.equal(find('.tags-list .tag-component .first-name').text(),'Jordan');
-    // assert.equal(currentRouteName(),'tags.list.index');
   });
 });
