85442358568191fbe7b907243b5ddb6cceaff078
Jordan Riser <riser.jordan@gmail.com>
2017-11-04 14:51:50 -0400

    only showing edit button on list page

--- tests/integration/components/tag-component-test.js
+++ tests/integration/components/tag-component-test.js
@@ -21,3 +21,10 @@ test('it sets a class to render a name tag background', function(assert) {
   this.render(hbs`{{tag-component data=data}}`);
   assert.dom('.template-1').exists();
 });
+
+test('"Edit" button only shows if "showEditButton" property is set to true', function(assert){
+  this.set('data',{template:1});
+
+  this.render(hbs`{{tag-component data=data}}`);
+  assert.equal(this.$().find('.edit-button').length, 0);
+});
