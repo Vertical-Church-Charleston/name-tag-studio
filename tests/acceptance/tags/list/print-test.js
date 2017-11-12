e9d6a6699bb984a615ce3241561a6f9e34bb7c4c
Jordan Riser <riser.jordan@gmail.com>
2017-11-12 18:46:32 -0500

    fix print page tes

--- tests/acceptance/tags/list/print-test.js
+++ tests/acceptance/tags/list/print-test.js
@@ -4,10 +4,9 @@ import moduleForAcceptance from 'name-tags/tests/helpers/module-for-acceptance';
 moduleForAcceptance('Acceptance | tags/index/new');
 
 test('should display a list of tags to print', function(assert) {
-  visit('/tags/new');
+  visit('/tags/print');
 
   andThen(function() {
-    assert.equal( find('.input-section').length, 1 );
-    assert.equal( find('.display-section').length, 1 );
+    assert.equal( find('.print-section').length, 1 );
   });
 });
