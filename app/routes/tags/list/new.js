bb80f462d97ff18131bfba80003f1947d7540051
Jordan Riser <riser.jordan@gmail.com>
2017-11-21 00:13:33 -0500

    adding animation for adding new name tag

--- app/routes/tags/list/new.js
+++ app/routes/tags/list/new.js
@@ -1,6 +1,8 @@
+import $ from 'jquery';
 import Route from '@ember/routing/route';
 import AnimateOutMixin from 'name-tags/mixins/animate-out';
 import { inject as service } from '@ember/service';
+import { later } from '@ember/runloop';
 
 export default Route.extend(AnimateOutMixin,{
   printList: service(),
@@ -13,15 +15,40 @@ export default Route.extend(AnimateOutMixin,{
   },
   actions: {
     create(){
-      var newTag = this.store.createRecord('tag',this.controller.get('model'));
-      newTag.save().then(()=>{
-        this.get('printList.list').pushObject(newTag);
-        this.controller.set('model',{
-          firstName: '',
-          lastName: '',
-          template: 1
-        });
+      let tagComponent = $('.display-section .tag-component');
+      let printListComponent = $('.print-list');
+      // console.log(tagComponent.offset().top - $(window).scrollTop());
+      tagComponent.animate({
+        'top': (printListComponent.offset().top - tagComponent.offset().top + 25) - (tagComponent.outerHeight() / 2),
+        'left': (printListComponent.offset().left - tagComponent.offset().left + 25) - (tagComponent.outerWidth() / 2)
+      }, 350, 'swing').css({
+        'transform': 'scale(0.09)'
       });
+      later(() => {
+        var newTag = this.store.createRecord('tag',this.controller.get('model'));
+        newTag.save().then(()=>{
+          this.get('printList.list').pushObject(newTag);
+          this.controller.set('model',{
+            firstName: '',
+            lastName: '',
+            template: 1
+          });
+          later(() => {
+            tagComponent.css({
+              'opacity': 0,
+              'top': 'initial',
+              'left': 'initial',
+              'transform': 'scale(1) translateY(-100vh)'
+            });
+            later(() => {
+              tagComponent.css({
+                'opacity': 1,
+                'transform': 'translateY(0)'
+              });
+            },150);
+          },250);
+        });
+      }, 350);
     }
   }
 });
