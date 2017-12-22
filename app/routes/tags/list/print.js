884ffde7a58f8ed88115857fab19d815236253e5
Jordan Riser <riser.jordan@gmail.com>
2017-12-22 16:15:13 -0500

    install fetch and hit print endpoint

--- app/routes/tags/list/print.js
+++ app/routes/tags/list/print.js
@@ -1,4 +1,32 @@
 import Route from '@ember/routing/route';
+import ENV from 'name-tags/config/environment';
+import fetch from 'fetch';
+import $ from 'jquery';
+
+const showFile = (blob)=>{
+  // It is necessary to create a new blob object with mime-type explicitly set
+  // otherwise only Chrome works like it should
+  var newBlob = new Blob([blob], {type: "application/pdf"})
+
+  // IE doesn't allow using a blob object directly as link href
+  // instead it is necessary to use msSaveOrOpenBlob
+  if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+    window.navigator.msSaveOrOpenBlob(newBlob);
+    return;
+  } 
+
+  // For other browsers: 
+  // Create a link pointing to the ObjectURL containing the blob.
+  const data = window.URL.createObjectURL(newBlob);
+  var link = document.createElement('a');
+  link.href = data;
+  link.download="file.pdf";
+  link.click();
+  setTimeout(()=>{
+    // For Firefox it is necessary to delay revoking the ObjectURL
+    window.URL.revokeObjectURL(data);
+  }, 100)
+}
 
 export default Route.extend({
   actions: {
@@ -9,6 +37,19 @@ export default Route.extend({
       if(!this.controller.get('printList.list.length')){
         this.transitionTo('tags.list.index')
       }
+    },
+    printTags(){
+      const tagParams = $.param({tags: this.controller.get('printList.listIds')})
+      fetch(`${ENV.APP.apiUrl}${ENV.APP.apiNameSpace}/printtags?${tagParams}`,{
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/pdf'
+        }
+      })
+      // .then(r => r.blob())
+      // .then(showFile);
+      .then(response => response.blob())
+      .then(data => window.open(URL.createObjectURL(data)))
     }
   }
 });
