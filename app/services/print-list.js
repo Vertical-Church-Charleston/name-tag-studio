87475c6ead0e3766d58fc659f0cc2664b0164166
Jordan Riser <riser.jordan@gmail.com>
2017-12-22 16:15:36 -0500

    creating computed list of just tag ids

--- app/services/print-list.js
+++ app/services/print-list.js
@@ -1,6 +1,10 @@
 import Service from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Service.extend({
+  listIds: computed('list.[]', function(){
+    return this.get('list').mapBy('id');
+  }),
   init() {
     this._super(...arguments);
     this.list = [];
